50e0fc7f6eb8be06cdab529b0b3636147eea1cb2
Yulia C <yulka1355@gmail.com>
2025-06-14 17:20:54 +0300

    Create epmty cmps place holders for jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,7 +4,6 @@ export function Home() {
     return (
         <section className="home container flex justify-center">
             <h1>Welcome to Miss Books</h1>
-            {/* <img src="./assets/img/1.jpg" alt="hero-image" /> */}
         </section>
     )
 }
