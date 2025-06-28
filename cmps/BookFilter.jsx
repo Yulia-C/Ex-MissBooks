2ed90968ddf2c26a005268674c5921ab04fbb43b
Yulia C <yulka1355@gmail.com>
2025-06-28 14:51:16 +0300

    Add debounce

--- cmps/BookFilter.jsx
+++ cmps/BookFilter.jsx
@@ -1,12 +1,16 @@
-const { useEffect, useState } = React
+const { useEffect, useState, useRef } = React
+import { debounce } from "../services/util.service.js"
 import { bookService } from "../services/book.service.js"
-export function BookFilter({ defaultFilter, onSetFilter }) {
+export function BookFilter({ defaultFilter, onSetFilterBy }) {
 
     const [filterByToEdit, setFilterByToEdit] = useState({ ...defaultFilter })
+    // const onSetFilterByDebounce = debounce(onSetFilterBy, 500)
+
+    const onSetFilterByDebounce = useRef(debounce(onSetFilterBy, 500)).current
     const { txt, minPrice } = filterByToEdit
 
     useEffect(() => {
-        onSetFilter(filterByToEdit)
+        onSetFilterByDebounce(filterByToEdit)
     }, [filterByToEdit])
 
     const [categories, setCategories] = useState(null)
@@ -16,6 +20,7 @@ export function BookFilter({ defaultFilter, onSetFilter }) {
     }, [categories])
 
     function loadCategories() {
+
         bookService.getCategories()
             .then(categories => {
                 setCategories(categories)
@@ -39,29 +44,30 @@ export function BookFilter({ defaultFilter, onSetFilter }) {
     }
 
     return (
-        <section className="book-filter">
-            <h2>Filer books</h2>
-            <form>
-                <label htmlFor='txt'>Search anything</label>
-                <input onChange={handleChange} value={txt} id='txt' name='txt' type='text' />
 
-                <label htmlFor='minPrice'>Min Price</label>
-                <input onChange={handleChange} value={minPrice || ''} id='minPrice' name='minPrice' type='range' min='80' max='500' title={minPrice} />
-            </form>
-            <section className="categories">
-                <h4>Book categories:</h4>
-                <ul className="clean-list">
-                    {categories && categories.map(category =>
-                        <li onClick={() => {
-                            const target = {
-                                name: 'txt',
-                                value: category
-                            }
-                            handleChange({ target })
-                        }} key={category}>{category}</li>)}
-                </ul>
+            <section className="book-filter">
+                <h2>Filer books</h2>
+                <form>
+                    <label htmlFor='txt'>Search anything</label>
+                    <input onChange={handleChange} value={txt} id='txt' name='txt' type='text' />
+
+                    <label htmlFor='minPrice'>Min Price</label>
+                    <input onChange={handleChange} value={minPrice || ''} id='minPrice' name='minPrice' type='range' min='80' max='500' title={minPrice} />
+                </form>
+                <section className="categories">
+                    <h4>Book categories:</h4>
+                    <ul className="clean-list">
+                        {categories && categories.map(category =>
+                            <li onClick={() => {
+                                const target = {
+                                    name: 'txt',
+                                    value: category
+                                }
+                                handleChange({ target })
+                            }} key={category}>{category}</li>)}
+                    </ul>
+                </section>
+
             </section>
-
-        </section>
     )
 }
